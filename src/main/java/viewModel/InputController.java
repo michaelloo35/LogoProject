da607716031f8b5379f9cc1663a5eb2f5ec7123b
michaelloo35 <michaelloo35@gmail.com>
2017-11-28 12:50:41 +0100

    Added boar

--- src/main/java/viewModel/InputController.java
+++ src/main/java/viewModel/InputController.java
@@ -1,19 +1,28 @@
 package viewModel;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 
 public class InputController {
 
+
     @FXML
     private TextField textField;
 
     @FXML
+    private Canvas canvas;
+
+    @FXML
     private TextArea textArea;
+
+    private GraphicsContext gc;
 
     @FXML
     public void onEnter(KeyEvent ke) {
@@ -21,6 +30,11 @@ public class InputController {
             String message = textField.getText();
             textArea.appendText(message + "\n");
             textField.clear();
+            gc = canvas.getGraphicsContext2D();
+            Image image = new Image(getClass().getClassLoader().getResourceAsStream("boar.png"));
+            gc.drawImage(image,0,0);
         }
     }
+
+
 }
