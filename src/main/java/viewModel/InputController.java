9f801fa483344e238c04e64569ca80d0bafab1c2
michaelloo35 <michaelloo35@gmail.com>
2017-11-28 12:36:01 +0100

    Text Area handling

--- src/main/java/viewModel/InputController.java
+++ src/main/java/viewModel/InputController.java
@@ -1,6 +1,7 @@
 package viewModel;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -12,9 +13,13 @@ public class InputController {
     private TextField textField;
 
     @FXML
-    public void onEnter(KeyEvent ke){
-        if (ke.getCode().equals(KeyCode.ENTER))
-        {
+    private TextArea textArea;
+
+    @FXML
+    public void onEnter(KeyEvent ke) {
+        if (ke.getCode().equals(KeyCode.ENTER)) {
+            String message = textField.getText();
+            textArea.appendText(message + "\n");
             textField.clear();
         }
     }
