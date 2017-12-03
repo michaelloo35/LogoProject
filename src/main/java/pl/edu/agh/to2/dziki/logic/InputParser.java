ce81745b42af2fc6a0921d40dff50e2a2cee021a
michaelloo35 <michaelloo35@gmail.com>
2017-12-03 15:05:15 +0100

    Added method signature

--- src/main/java/pl/edu/agh/to2/dziki/logic/InputParser.java
+++ src/main/java/pl/edu/agh/to2/dziki/logic/InputParser.java
@@ -1,4 +1,12 @@
 package pl.edu.agh.to2.dziki.logic;
 
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
 public class InputParser {
+
+    public List<String> parse(String input) {
+        return emptyList();
+    }
 }
