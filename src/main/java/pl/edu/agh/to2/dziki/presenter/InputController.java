c3a34547b646e775c4565d9e80e47ae5c8409f2d
michaelloo35 <michaelloo35@gmail.com>
2017-12-12 06:20:47 +0100

    Fix

--- src/main/java/pl/edu/agh/to2/dziki/presenter/InputController.java
+++ src/main/java/pl/edu/agh/to2/dziki/presenter/InputController.java
@@ -7,12 +7,19 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import pl.edu.agh.to2.dziki.model.Boar;
+import pl.edu.agh.to2.dziki.model.InputInterpreter;
+import pl.edu.agh.to2.dziki.model.task.Task;
 import pl.edu.agh.to2.dziki.presenter.parser.InputParser;
+
+import java.util.List;
 
 
 public class InputController {
 
     private InputParser inputParser;
+    private InputInterpreter inputInterpreter;
+    private TaskExecutor executor;
+    private ViewUpdater viewUpdater;
 
     @FXML
     private TextField textField;
@@ -28,9 +35,14 @@ public class InputController {
 
     private Boar boar;
 
+
     @FXML
     public void initialize() {
-        boar = new Boar(new ViewUpdater(boarLayer, drawLayer));
+        inputParser = new InputParser();
+        inputInterpreter = new InputInterpreter();
+        viewUpdater = new ViewUpdater(boarLayer, drawLayer);
+        boar = new Boar(viewUpdater);
+        executor = new TaskExecutor(boar, viewUpdater);
     }
 
     @FXML
@@ -38,38 +50,11 @@ public class InputController {
         if (ke.getCode().equals(KeyCode.ENTER)) {
             String message = textField.getText();
             textArea.appendText(message + "\n");
-            switch (message) {
-                case "start":
-                    boar.initialize();
-                    break;
-                case "r":
-                    boar.rotate(10);
-                    break;
-                case "rr":
-                    boar.rotate(-10);
-                    break;
-                case "f":
-                    boar.moveForward(10);
-                    break;
-                case "b":
-                    boar.moveBackward(10);
-                    break;
-                case "h":
-                    boar.hide();
-                    break;
-                case "s":
-                    boar.show();
-                    break;
-                case "lift":
-                    boar.lift();
-                    break;
-                case "lower":
-                    boar.lower();
-                    break;
-
-            }
+            List<String> validatedInput = inputParser.parse(message);
+            List<Task> tasks = inputInterpreter.interpretAndGenerateTasks(validatedInput);
+            executor.executeTasks(tasks);
 //            TODO commented for testing purposes
-//            textField.clear();
+            textField.clear();
         }
 
     }
