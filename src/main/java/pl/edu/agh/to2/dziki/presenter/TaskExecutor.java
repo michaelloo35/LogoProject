feca7a75b9fcbd5588e57232871359bcdcbe37da
michaelloo35 <michaelloo35@gmail.com>
2017-12-12 06:20:14 +0100

    Controller update

---
+++ src/main/java/pl/edu/agh/to2/dziki/presenter/TaskExecutor.java
@@ -0,0 +1,21 @@
+package pl.edu.agh.to2.dziki.presenter;
+
+import pl.edu.agh.to2.dziki.model.Boar;
+import pl.edu.agh.to2.dziki.model.task.Task;
+
+import java.util.List;
+
+public class TaskExecutor {
+
+    private final Boar boar;
+    private final ViewUpdater viewUpdater;
+
+    public TaskExecutor(Boar boar, ViewUpdater viewUpdater) {
+        this.boar = boar;
+        this.viewUpdater = viewUpdater;
+    }
+
+    public void executeTasks(List<Task> tasks) {
+        tasks.forEach(task -> task.execute(boar, viewUpdater));
+    }
+}
